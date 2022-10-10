bf712a49eeb7a276973447ffccbfbf3699015c3e
seonpilKim <ksp970306@gmail.com>
2022-10-10 16:17:57 +0900

    initial commit

---
+++ src/main/java/inha/tnt/hbc/HbcApplication.java
@@ -0,0 +1,13 @@
+package inha.tnt.hbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inha/tnt/hbc/HbcApplicationTests.java
@@ -0,0 +1,13 @@
+package inha.tnt.hbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

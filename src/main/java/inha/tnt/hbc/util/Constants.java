6bb672210b1b445acc4246a77baaf324d9350941
seonpilKim <ksp970306@gmail.com>
2022-10-10 17:59:07 +0900

    Feat: 상수 클래스 추가

---
+++ src/main/java/inha/tnt/hbc/util/Constants.java
@@ -0,0 +1,23 @@
+package inha.tnt.hbc.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+
+	public static final String ALL_PATTERN = "/**";
+
+	public static final String BREAK_TAG = "<br>";
+
+	public static final String DASH = "-";
+	public static final String EMPTY = "";
+	public static final String SPACE = " ";
+	public static final String DELIMITER = "_";
+	public static final String COMMA = ",";
+	public static final String DOT = ".";
+
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+	public static final String CONTENT_TYPE_HEADER = "Content-type";
+
+}

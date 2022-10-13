7b537b6fbe35a188ecc780355c0a7da557f708b6
seonpilKim <ksp970306@gmail.com>
2022-10-13 17:56:58 +0900

    Feat: 상수 추가

--- src/main/java/inha/tnt/hbc/util/Constants.java
+++ src/main/java/inha/tnt/hbc/util/Constants.java
@@ -16,8 +16,16 @@ public final class Constants {
 	public static final String DELIMITER = "_";
 	public static final String COMMA = ",";
 	public static final String DOT = ".";
+	public static final String BACK_SLASH = "\\";
+	public static final String SLASH = "/";
 
 	public static final String AUTHORIZATION_HEADER = "Authorization";
 	public static final String CONTENT_TYPE_HEADER = "Content-type";
+
+	public static final String ROOT_DIRECTORY = System.getProperty("user.dir");
+	public static final String TEMPORAL_DIRECTORY = "upload";
+
+	public static final String S3_DIRECTORY_MEMBER = "member";
+	public static final String S3_DIRECTORY_ROOM = "room";
 
 }

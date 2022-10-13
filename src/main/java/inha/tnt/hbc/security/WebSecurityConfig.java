3c4af18b455ce3e6d7fc1bc9e4ca7ac7cfd6a203
seonpilKim <ksp970306@gmail.com>
2022-10-13 18:56:16 +0900

    Feat: WebSecurityConfig 설정 업데이트

--- src/main/java/inha/tnt/hbc/security/WebSecurityConfig.java
+++ src/main/java/inha/tnt/hbc/security/WebSecurityConfig.java
@@ -2,19 +2,33 @@ package inha.tnt.hbc.security;
 
 import static inha.tnt.hbc.util.Constants.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import inha.tnt.hbc.security.jwt.JwtAuthenticationFilter;
+import inha.tnt.hbc.security.jwt.JwtAuthenticationProvider;
+import inha.tnt.hbc.security.matcher.SkipPathRequestMatcher;
+import inha.tnt.hbc.security.oauth2.CustomOAuth2UserService;
+import inha.tnt.hbc.security.oauth2.OAuth2SuccessHandler;
+import inha.tnt.hbc.util.JwtUtils;
 import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity
@@ -24,19 +38,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] AUTH_WHITELIST_SWAGGER = {"/v2/api-docs", "/configuration/ui",
 		"/swagger-resources/**", "/configuration/security", "/swagger-ui.html/**", "/webjars/**", "/swagger/**"};
 	private static final String[] AUTH_WHITELIST_GUEST = {"/", "/csrf", "/error", "/auth/**", "/oauth2/**"};
-	private static final String OAUTH2_REDIRECT_URI = "/oauth2/callback/*";
+	private static final String OAUTH2_REDIRECT_URI = "/oauth2/signin/*";
 
-	/*private final JwtUtils jwtUtils;
-	private final JwtAuthenticationProvider jwtAuthenticationProvider;*/
+	private final JwtUtils jwtUtils;
+	private final JwtAuthenticationProvider jwtAuthenticationProvider;
 	private final CustomAccessDeniedHandler accessDeniedHandler;
 	private final CustomAuthenticationEntryPoint authenticationEntryPoint;
-	/*private final OAuth2SuccessHandler oAuth2SuccessHandler;
+	private final OAuth2SuccessHandler oAuth2SuccessHandler;
 	private final CustomOAuth2UserService oAuth2UserService;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) {
-		auth.authenticationProvider(jwtAuthenticationProvider);
-	}*/
 
 	@Override
 	public void configure(WebSecurity web) {
@@ -65,7 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return source;
 	}
 
-	/*@Bean
+	@Bean
 	public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
 		final List<String> skipPaths = new ArrayList<>();
 		skipPaths.addAll(Arrays.stream(AUTH_WHITELIST_SWAGGER).collect(Collectors.toList()));
@@ -75,7 +84,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		filter.setAuthenticationManager(super.authenticationManager());
 		filter.setAuthenticationFailureHandler(authenticationEntryPointFailureHandler());
 		return filter;
-	}*/
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(jwtAuthenticationProvider);
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -102,16 +116,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and()
 
-			/*.oauth2Login()
+			.oauth2Login()
 			.redirectionEndpoint().baseUri(OAUTH2_REDIRECT_URI)
 			.and()
 
-			successHandler(oAuth2SuccessHandler)
+			.successHandler(oAuth2SuccessHandler)
 			.failureHandler(authenticationEntryPointFailureHandler())
-			.userInfoEndpoint().userService(oAuth2UserService)*/
+			.userInfoEndpoint().userService(oAuth2UserService)
 		;
 
-		// http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 }

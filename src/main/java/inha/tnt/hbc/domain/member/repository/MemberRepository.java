46a047575c04df65a16311325a425609373ef47d
seonpilKim <ksp970306@gmail.com>
2022-10-14 05:14:34 +0900

    Feat: MemberRepository 메소드 추가

--- src/main/java/inha/tnt/hbc/domain/member/repository/MemberRepository.java
+++ src/main/java/inha/tnt/hbc/domain/member/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package inha.tnt.hbc.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import inha.tnt.hbc.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByUsername(String username);
 
 }

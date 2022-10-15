536542276f66f8adf34ac75e76fe53f3aa3c8747
seonpilKim <ksp970306@gmail.com>
2022-10-15 15:36:23 +0900

    Feat: 간편 로그인 API 구현

--- src/main/java/inha/tnt/hbc/application/member/controller/OAuth2Controller.java
+++ src/main/java/inha/tnt/hbc/application/member/controller/OAuth2Controller.java
@@ -1,17 +1,26 @@
 package inha.tnt.hbc.application.member.controller;
 
+import static inha.tnt.hbc.model.ResultCode.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import inha.tnt.hbc.application.member.service.oauth2.OAuth2Service;
 import inha.tnt.hbc.model.ResultResponse;
 import inha.tnt.hbc.model.member.OAuth2Api;
+import inha.tnt.hbc.security.jwt.dto.JwtDto;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 public class OAuth2Controller implements OAuth2Api {
 
+	private final OAuth2Service oAuth2Service;
+
 	@Override
-	public ResponseEntity<ResultResponse> signin(String provider, String code) {
-		return null;
+	public ResponseEntity<ResultResponse> signin(String provider, String token) {
+		final JwtDto jwtDto = oAuth2Service.signin(provider, token);
+		return ResponseEntity.ok(ResultResponse.of(SIGNIN_SUCCESS, jwtDto));
 	}
 
 }
--- src/main/java/inha/tnt/hbc/model/member/OAuth2Api.java
+++ src/main/java/inha/tnt/hbc/model/member/OAuth2Api.java
@@ -1,16 +1,20 @@
 package inha.tnt.hbc.model.member;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import inha.tnt.hbc.model.ErrorResponse;
 import inha.tnt.hbc.model.ResultResponse;
+import inha.tnt.hbc.security.jwt.dto.JwtDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "OAuth2 회원 인증")
 @RequestMapping("/oauth2")
@@ -18,10 +22,16 @@ public interface OAuth2Api {
 
 	@ApiOperation(value = "간편 로그인")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name = "provider", value = "OAuth2 제공자", example = "kakao", required = true),
-		@ApiImplicitParam(name = "code", value = "인가 코드", example = "CKWmhCwdgVeZwZ2YNQAccr5aW1S6xItOGKkiCEepCj1zmgAAAYPDeqSi", required = true)
+		@ApiImplicitParam(name = "provider", value = "OAuth2 플랫폼명", example = "KAKAO", required = true),
+		@ApiImplicitParam(name = "token", value = "OAuth2 인가 토큰", example = "7POZ3TGhuFkBtaEj8WwFLB7Qm8WZvbSaf-lU5_C4CinJXgAAAYPWM1g9", required = true)
 	})
-	@GetMapping("/signin/{provider}")
-	ResponseEntity<ResultResponse> signin(@PathVariable String provider, @RequestParam String code);
+	@ApiResponses({
+		@ApiResponse(code = 1, response = JwtDto.class, message = ""
+			+ "status: 200 | code: R-M011 | message: 로그인에 성공하였습니다."),
+		@ApiResponse(code = 500, response = ErrorResponse.class, message = ""
+			+ "status: 500 | code: E-G001 | message: 내부 서버 오류입니다.")
+	})
+	@PostMapping("/signin/{provider}")
+	ResponseEntity<ResultResponse> signin(@PathVariable String provider, @RequestParam String token);
 
 }

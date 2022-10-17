418bf2a212385baa06d65f61e9a7c8756a22376e
seonpilKim <ksp970306@gmail.com>
2022-10-18 01:01:18 +0900

    Feat: 스웨거 basic 명세서 제거

--- src/main/java/inha/tnt/hbc/config/SwaggerConfig.java
+++ src/main/java/inha/tnt/hbc/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -29,7 +30,7 @@ public class SwaggerConfig {
 			.securityContexts(List.of(securityContext()))
 			.securitySchemes(List.of(apiKey()))
 			.select()
-			.apis(RequestHandlerSelectors.any())
+			.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 			.paths(PathSelectors.any())
 			.build();
 	}

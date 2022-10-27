12665035c7065b399ef8b03de4710db8ccbd786d
seonpilKim <ksp970306@gmail.com>
2022-10-27 23:20:21 +0900

    Feat: Phone Validator 추가

---
+++ src/main/java/inha/tnt/hbc/validator/PhoneValidator.java
@@ -0,0 +1,50 @@
+package inha.tnt.hbc.validator;
+
+import static inha.tnt.hbc.model.ErrorCode.*;
+import static inha.tnt.hbc.model.ResultCode.*;
+
+import java.util.regex.Pattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.stereotype.Component;
+
+import inha.tnt.hbc.annotation.Phone;
+import inha.tnt.hbc.domain.member.service.MemberService;
+import inha.tnt.hbc.util.MessageTemplates;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class PhoneValidator implements ConstraintValidator<Phone, String> {
+
+	public static final String REGEXP = "^\\d{3}-\\d{3,4}-\\d{4}$";
+	private final MemberService memberService;
+
+	@Override
+	public void initialize(Phone constraintAnnotation) {
+	}
+
+	@Override
+	public boolean isValid(String phone, ConstraintValidatorContext context) {
+		context.disableDefaultConstraintViolation();
+		if (phone == null) {
+			context.buildConstraintViolationWithTemplate(NOT_NULL.getMessage())
+				.addConstraintViolation();
+			return false;
+		}
+		if (!Pattern.matches(REGEXP, phone)) {
+			context.buildConstraintViolationWithTemplate(MessageTemplates.getPatternMessage(REGEXP))
+				.addConstraintViolation();
+			return false;
+		}
+		if (memberService.isExistingPhone(phone)) {
+			context.buildConstraintViolationWithTemplate(PHONE_ALREADY_USED.getMessage())
+				.addConstraintViolation();
+			return false;
+		}
+		return true;
+	}
+
+}

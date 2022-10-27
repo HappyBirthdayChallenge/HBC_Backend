bd264222bd866b31ec0364e96709d72b9a514064
seonpilKim <ksp970306@gmail.com>
2022-10-28 03:10:08 +0900

    Feat: AsyncConfig 추가

---
+++ src/main/java/inha/tnt/hbc/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package inha.tnt.hbc.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+	@Override
+	public Executor getAsyncExecutor() {
+		final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(20);
+		executor.initialize();
+		return executor;
+	}
+
+}

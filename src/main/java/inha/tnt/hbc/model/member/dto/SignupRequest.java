f03b63b668f98c90b28e141de464f5be62c08e69
seonpilKim <ksp970306@gmail.com>
2022-10-27 17:44:40 +0900

    Feat: 회원가입 요청 모델 변경
    제거: 이메일
    추가: 휴대폰, 생일

--- src/main/java/inha/tnt/hbc/model/member/dto/SignupRequest.java
+++ src/main/java/inha/tnt/hbc/model/member/dto/SignupRequest.java
@@ -1,12 +1,13 @@
 package inha.tnt.hbc.model.member.dto;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import inha.tnt.hbc.annotation.BirthDay;
+import inha.tnt.hbc.vo.BirthDate;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -34,13 +35,15 @@ public class SignupRequest {
 	@ApiModelProperty(value = "비밀번호(영문자 && 숫자 && 특수문자)[10, 20]", example = "qlalfqjsgh1@", required = true)
 	private String passwordCheck;
 	@NotNull
-	@Email
-	@ApiModelProperty(value = "이메일", required = true, example = "example@gmail.com")
-	private String email;
+	@Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$")
+	@ApiModelProperty(value = "휴대폰", required = true, example = "010-9128-5708")
+	private String phone;
 	@NotNull
 	@Pattern(regexp = "^[가-힣A-Za-z\\d]{2,20}$")
 	@ApiModelProperty(value = "이름(한글 || 영문자 || 숫자)[2, 20]", example = "손흥민", required = true)
 	private String name;
+	@BirthDay
+	private BirthDate birthDate;
 	@NotNull
 	@Pattern(regexp = "^[A-Za-z\\d]{8}-[A-Za-z\\d]{4}-[A-Za-z\\d]{4}-[A-Za-z\\d]{4}-[A-Za-z\\d]{12}$")
 	@ApiModelProperty(value = "인증 키", example = "550e8400-e29b-41d4-a716-446655440000", required = true)

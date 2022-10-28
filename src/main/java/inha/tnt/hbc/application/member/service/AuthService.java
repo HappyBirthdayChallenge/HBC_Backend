f69f6beff7d582bce2737dabf7565bf4c93520c9
seonpilKim <ksp970306@gmail.com>
2022-10-28 22:30:23 +0900

    Feat: 토큰 재발급 로직 구현 및 로그인 로직 기능 추가
    로그인 시, 재발급 토큰 Redis에 저장하는 로직 추가

--- src/main/java/inha/tnt/hbc/application/member/service/AuthService.java
+++ src/main/java/inha/tnt/hbc/application/member/service/AuthService.java
@@ -1,8 +1,8 @@
 package inha.tnt.hbc.application.member.service;
 
 import static inha.tnt.hbc.domain.member.entity.MemberRoles.*;
-import static inha.tnt.hbc.model.ErrorCode.*;
 import static inha.tnt.hbc.model.ResultCode.*;
+import static inha.tnt.hbc.util.JwtUtils.*;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,12 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import inha.tnt.hbc.domain.member.entity.Member;
 import inha.tnt.hbc.domain.member.service.MemberService;
-import inha.tnt.hbc.exception.EntityNotFoundException;
+import inha.tnt.hbc.domain.member.service.TokenService;
 import inha.tnt.hbc.model.ResultResponse;
 import inha.tnt.hbc.security.jwt.dto.JwtDto;
 import inha.tnt.hbc.util.JwtUtils;
 import inha.tnt.hbc.vo.BirthDate;
 import inha.tnt.hbc.vo.Image;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -25,6 +26,7 @@ public class AuthService {
 	private final MemberService memberService;
 	private final PasswordEncoder passwordEncoder;
 	private final JwtUtils jwtUtils;
+	private final TokenService tokenService;
 
 	@Transactional
 	public Member signup(String username, String password, String name, String phone, BirthDate birthDate,
@@ -41,15 +43,19 @@ public class AuthService {
 		return memberService.save(member);
 	}
 
-	public JwtDto signin(String username, String password) {
+	public ResultResponse signin(String username, String password) {
 		final Member member = memberService.findByUsername(username);
+
 		if (!passwordEncoder.matches(password, member.getPassword())) {
-			throw new EntityNotFoundException(MEMBER_UNFOUNDED);
+			return ResultResponse.of(USERNAME_PASSWORD_INCORRECT);
 		}
-		return JwtDto.builder()
+
+		final JwtDto jwtDto = JwtDto.builder()
 			.accessToken(jwtUtils.generateAccessToken(member))
 			.refreshToken(jwtUtils.generateRefreshToken(member))
 			.build();
+		tokenService.saveRefreshToken(member.getId(), jwtDto.getRefreshToken());
+		return ResultResponse.of(SIGNIN_SUCCESS, jwtDto);
 	}
 
 	public ResultResponse checkUsername(String username) {
@@ -65,6 +71,27 @@ public class AuthService {
 			return ResultResponse.of(PHONE_AVAILABLE);
 		} else {
 			return ResultResponse.of(PHONE_ALREADY_USED);
+		}
+	}
+
+	public ResultResponse reissueToken(String refreshToken) {
+		try {
+			final String jwt = refreshToken.substring(TOKEN_PREFIX_LENGTH);
+			final Long memberId = jwtUtils.getMemberId(jwt);
+			final String token = tokenService.getRefreshToken(memberId);
+
+			if (token == null || !token.equals(jwt)) {
+				return ResultResponse.of(JWT_REISSUE_FAILURE);
+			}
+
+			final JwtDto jwtDto = JwtDto.builder()
+				.accessToken(jwtUtils.generateAccessToken(jwt))
+				.refreshToken(jwtUtils.generateRefreshToken(jwt))
+				.build();
+			tokenService.saveRefreshToken(memberId, jwtDto.getRefreshToken());
+			return ResultResponse.of(JWT_REISSUE_SUCCESS, jwtDto);
+		} catch (JwtException e) {
+			return ResultResponse.of(JWT_REISSUE_FAILURE);
 		}
 	}
 

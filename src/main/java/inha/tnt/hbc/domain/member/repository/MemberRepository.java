4596da543eecf7fe990849c4a47c08fc90b52830
seonpilKim <ksp970306@gmail.com>
2022-10-28 23:12:27 +0900

    Feat: Name, Phone으로 회원 조회 쿼리 메소드 추가

--- src/main/java/inha/tnt/hbc/domain/member/repository/MemberRepository.java
+++ src/main/java/inha/tnt/hbc/domain/member/repository/MemberRepository.java
@@ -12,4 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByPhone(String phone);
 
+	Optional<Member> findByNameAndPhone(String name, String phone);
+
 }

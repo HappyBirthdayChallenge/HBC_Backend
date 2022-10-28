a8d282c1772e758c520b7b59323293a949231ee9
seonpilKim <ksp970306@gmail.com>
2022-10-28 23:16:18 +0900

    Feat: 요청 모델 변경 (이메일 -> 휴대폰 번호)

--- src/main/java/inha/tnt/hbc/model/member/dto/FindUsernameRequest.java
+++ src/main/java/inha/tnt/hbc/model/member/dto/FindUsernameRequest.java
@@ -1,9 +1,9 @@
 package inha.tnt.hbc.model.member.dto;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import inha.tnt.hbc.annotation.Phone;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,10 +21,9 @@ public class FindUsernameRequest {
 	@Pattern(regexp = "^[가-힣A-Za-z\\d]{2,20}$")
 	@ApiModelProperty(value = "이름(한글 || 영문자 || 숫자)[2, 20]", example = "손흥민", required = true)
 	private String name;
-	@NotNull
-	@Email
-	@ApiModelProperty(value = "이메일", required = true, example = "example@gmail.com")
-	private String email;
+	@Phone
+	@ApiModelProperty(value = "휴대폰 번호", required = true, example = "010-9128-5708")
+	private String phone;
 	@NotNull
 	@Pattern(regexp = "^[A-Za-z\\d]{8}-[A-Za-z\\d]{4}-[A-Za-z\\d]{4}-[A-Za-z\\d]{4}-[A-Za-z\\d]{12}$")
 	@ApiModelProperty(value = "인증 키", example = "550e8400-e29b-41d4-a716-446655440000", required = true)

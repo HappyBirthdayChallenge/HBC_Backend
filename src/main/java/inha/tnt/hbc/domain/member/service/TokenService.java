d4dae699abf0f1d80939d3b4c07a1c555ba444dc
seonpilKim <ksp970306@gmail.com>
2022-10-28 22:24:39 +0900

    Feat: Redis 토큰 관리 서비스 추가

---
+++ src/main/java/inha/tnt/hbc/domain/member/service/TokenService.java
@@ -0,0 +1,35 @@
+package inha.tnt.hbc.domain.member.service;
+
+import static inha.tnt.hbc.util.Constants.*;
+import static java.util.concurrent.TimeUnit.*;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+	private static final String REDIS_PREFIX_KEY = "rt";
+	private final RedisTemplate<String, String> redisTemplate;
+	@Value("${jwt.valid.refresh}")
+	private long REFRESH_TOKEN_VALIDITY;
+
+	@Async
+	public void saveRefreshToken(Long memberId, String refreshToken) {
+		redisTemplate.opsForValue().set(generateRedisKey(memberId), refreshToken, REFRESH_TOKEN_VALIDITY, MILLISECONDS);
+	}
+
+	public String getRefreshToken(Long memberId) {
+		return redisTemplate.opsForValue().get(generateRedisKey(memberId));
+	}
+
+	private String generateRedisKey(Long key) {
+		return REDIS_PREFIX_KEY + DELIMITER + key;
+	}
+
+}

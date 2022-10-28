35eb2559dcdb726896e881c67ea0c1fcffd1a610
seonpilKim <ksp970306@gmail.com>
2022-10-29 02:24:52 +0900

    Feat: 인증 키 생성 테스트 API 일반화 -> 인증 유형별 요청 데이터 추가

--- src/main/java/inha/tnt/hbc/application/member/controller/TestAuthController.java
+++ src/main/java/inha/tnt/hbc/application/member/controller/TestAuthController.java
@@ -1,5 +1,6 @@
 package inha.tnt.hbc.application.member.controller;
 
+import static inha.tnt.hbc.util.Constants.*;
 import static java.util.concurrent.TimeUnit.*;
 
 import java.util.UUID;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import inha.tnt.hbc.domain.member.service.IdentityVerificationService.IdentityVerificationTypes;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
@@ -27,12 +30,16 @@ public class TestAuthController {
 	@Value("${auth.valid.key}")
 	private long AUTH_KEY_VALIDITY;
 
-	@ApiOperation(value = "회원가입 인증 키 생성")
-	@ApiImplicitParam(name = "phone", value = "휴대폰 번호", required = true, example = "010-9128-5708")
+	@ApiOperation(value = "인증 키 생성")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "phone", value = "휴대폰 번호", required = true, example = "010-9128-5708"),
+		@ApiImplicitParam(name = "type", value = "인증 유형", required = true, example = "SIGNUP")
+	})
 	@PostMapping("/key/generate")
-	public String generateAuthKey(@RequestParam String phone) {
+	public String generateAuthKey(@RequestParam String phone, @RequestParam IdentityVerificationTypes type) {
 		final String authKey = UUID.randomUUID().toString();
-		redisTemplate.opsForValue().set("iv_su_" + authKey, phone, AUTH_KEY_VALIDITY, MILLISECONDS);
+		redisTemplate.opsForValue()
+			.set("iv_" + type.getInfix() + DELIMITER + authKey, phone, AUTH_KEY_VALIDITY, MILLISECONDS);
 		return authKey;
 	}
 

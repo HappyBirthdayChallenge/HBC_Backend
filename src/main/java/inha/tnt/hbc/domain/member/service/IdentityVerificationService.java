11b031bc1ca03ec4c3fedb5400a3ffbddd1c33c8
seonpilKim <ksp970306@gmail.com>
2022-10-28 22:24:05 +0900

    Feat: 인증코드, 인증키 -> Redis 저장 키에 접두사 추가

--- src/main/java/inha/tnt/hbc/domain/member/service/IdentityVerificationService.java
+++ src/main/java/inha/tnt/hbc/domain/member/service/IdentityVerificationService.java
@@ -1,42 +1,46 @@
 package inha.tnt.hbc.domain.member.service;
 
+import static inha.tnt.hbc.util.Constants.*;
 import static java.util.concurrent.TimeUnit.*;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
+// TODO: [Refactor] RedisAdapter
 @Service
 @RequiredArgsConstructor
 public class IdentityVerificationService {
 
+	private static final String REDIS_PREFIX_KEY = "iv";
 	private final RedisTemplate<String, String> redisTemplate;
 	@Value("${auth.valid.code}")
 	private long AUTH_CODE_VALIDITY;
 	@Value("${auth.valid.key}")
 	private long AUTH_KEY_VALIDITY;
 
-	@Transactional(readOnly = true)
 	public boolean isValid(String key, String value) {
-		return redisTemplate.opsForValue().get(key) != null && String.valueOf(redisTemplate.opsForValue().get(key)).equals(value);
+		final String fullKey = generateRedisKey(key);
+		return redisTemplate.opsForValue().get(fullKey) != null &&
+			String.valueOf(redisTemplate.opsForValue().get(fullKey)).equals(value);
 	}
 
-	@Transactional
 	public void delete(String key) {
-		redisTemplate.delete(key);
+		redisTemplate.delete(generateRedisKey(key));
 	}
 
-	@Transactional
 	public void saveAuthCode(String code, String phone) {
-		redisTemplate.opsForValue().set(code, phone, AUTH_CODE_VALIDITY, MILLISECONDS);
+		redisTemplate.opsForValue().set(generateRedisKey(code), phone, AUTH_CODE_VALIDITY, MILLISECONDS);
 	}
 
-	@Transactional
 	public void saveAuthKey(String key, String phone) {
-		redisTemplate.opsForValue().set(key, phone, AUTH_KEY_VALIDITY, MILLISECONDS);
+		redisTemplate.opsForValue().set(generateRedisKey(key), phone, AUTH_KEY_VALIDITY, MILLISECONDS);
+	}
+
+	private String generateRedisKey(String key) {
+		return REDIS_PREFIX_KEY + DELIMITER + key;
 	}
 
 }

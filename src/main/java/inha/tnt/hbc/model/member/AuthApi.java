5f4fc4ee31777ac86a12dfff2bc3bf0b285301bb
seonpilKim <ksp970306@gmail.com>
2022-10-28 23:12:09 +0900

    Feat: 본인 여부 확인 API 명세서 추가

--- src/main/java/inha/tnt/hbc/model/member/AuthApi.java
+++ src/main/java/inha/tnt/hbc/model/member/AuthApi.java
@@ -64,7 +64,7 @@ public interface AuthApi {
 	@ApiOperation(value = "아이디 유효성 확인")
 	@ApiResponses({
 		@ApiResponse(code = 1, response = Void.class, message = ""
-			+ "status: 200 | code: R-M001 | message: 사용 가능한 아이디입니다."
+			+ "status: 200 | code: R-M001 | message: 사용 가능한 아이디입니다.\n"
 			+ "status: 200 | code: R-M004 | message: 이미 사용하고 있는 아이디입니다."),
 		@ApiResponse(code = 500, response = ErrorResponse.class, message = ""
 			+ "status: 400 | code: E-G002 | message: 입력 값이 유효하지 않습니다.\n"
@@ -76,7 +76,7 @@ public interface AuthApi {
 	@ApiOperation(value = "휴대폰 번호 유효성 확인")
 	@ApiResponses({
 		@ApiResponse(code = 1, response = Void.class, message = ""
-			+ "status: 200 | code: R-M015 | message: 사용 가능한 휴대폰 번호입니다."
+			+ "status: 200 | code: R-M015 | message: 사용 가능한 휴대폰 번호입니다.\n"
 			+ "status: 200 | code: R-M005 | message: 이미 사용하고 있는 휴대폰 번호입니다."),
 		@ApiResponse(code = 500, response = ErrorResponse.class, message = ""
 			+ "status: 400 | code: E-G002 | message: 입력 값이 유효하지 않습니다.\n"
@@ -115,7 +115,7 @@ public interface AuthApi {
 		+ "3. 가입 중간에 다른 회원이 먼저 동일한 아이디 or 휴대폰번호로 가입하는 예외 상황도 고려해 주세요. (status: 400)")
 	@ApiResponses({
 		@ApiResponse(code = 1, response = Void.class, message = ""
-			+ "status: 200 | code: R-M003 | message: 회원가입에 성공하였습니다."
+			+ "status: 200 | code: R-M003 | message: 회원가입에 성공하였습니다.\n"
 			+ "status: 200 | code: R-IV003 | message: 유효하지 않은 인증 키입니다."),
 		@ApiResponse(code = 500, response = ErrorResponse.class, message = ""
 			+ "status: 400 | code: E-G002 | message: 입력 값이 유효하지 않습니다.\n"
@@ -125,6 +125,14 @@ public interface AuthApi {
 	ResponseEntity<ResultResponse> signup(@Valid @RequestBody SignupRequest request);
 
 	@ApiOperation(value = "본인 여부 확인")
+	@ApiResponses({
+		@ApiResponse(code = 1, response = Void.class, message = ""
+			+ "status: 200 | code: R-M016 | message: 본인 여부 확인에 성공하였습니다\n."
+			+ "status: 200 | code: R-M017 | message: 본인 여부 확인에 실패하였습니다"),
+		@ApiResponse(code = 500, response = ErrorResponse.class, message = ""
+			+ "status: 400 | code: E-G002 | message: 입력 값이 유효하지 않습니다.\n"
+			+ "status: 500 | code: E-G001 | message: 내부 서버 오류입니다.")
+	})
 	@PostMapping("/identify")
 	ResponseEntity<ResultResponse> identify(@Valid @RequestBody IdentifyRequest request);
 
--- src/main/java/inha/tnt/hbc/model/member/dto/IdentifyRequest.java
+++ src/main/java/inha/tnt/hbc/model/member/dto/IdentifyRequest.java
@@ -1,9 +1,9 @@
 package inha.tnt.hbc.model.member.dto;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import inha.tnt.hbc.annotation.Phone;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,9 +21,8 @@ public class IdentifyRequest {
 	@Pattern(regexp = "^[가-힣A-Za-z\\d]{2,20}$")
 	@ApiModelProperty(value = "이름(한글 || 영문자 || 숫자)[2, 20]", example = "손흥민", required = true)
 	private String name;
-	@NotNull
-	@Email
-	@ApiModelProperty(value = "이메일", required = true, example = "example@gmail.com")
-	private String email;
+	@Phone
+	@ApiModelProperty(value = "휴대폰 번호", required = true, example = "010-9128-5708")
+	private String phone;
 
 }

71c37c1622653a74239b7657adc43343d5e13139
seonpilKim <ksp970306@gmail.com>
2022-11-03 23:04:23 +0900

    Feat: OAuth2Account - Member 영속성 전이(Remove) 적용

--- src/main/java/inha/tnt/hbc/domain/member/entity/Member.java
+++ src/main/java/inha/tnt/hbc/domain/member/entity/Member.java
@@ -1,20 +1,25 @@
 package inha.tnt.hbc.domain.member.entity;
 
 import static inha.tnt.hbc.util.Constants.*;
+import static javax.persistence.CascadeType.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import inha.tnt.hbc.domain.BaseEntity;
+import inha.tnt.hbc.domain.member.entity.oauth2.OAuth2Account;
 import inha.tnt.hbc.domain.member.exception.AlreadySetupBirthDateException;
 import inha.tnt.hbc.vo.BirthDate;
 import inha.tnt.hbc.vo.Image;
@@ -44,6 +49,8 @@ public class Member extends BaseEntity {
 	private Image image;
 	private String phone;
 	private String authorities;
+	@OneToMany(mappedBy = "member", cascade = REMOVE)
+	private List<OAuth2Account> oAuth2Accounts = new ArrayList<>();
 
 	public void setupBirthDate(BirthDate birthDate) {
 		if (!this.birthDate.isInitial()) {

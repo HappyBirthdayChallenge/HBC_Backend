9079e7a91496573d1aaacf012a1f36076d6442f6
seonpilKim <ksp970306@gmail.com>
2022-11-12 20:15:08 +0900

    Feat: RoomRepository 생성

---
+++ src/main/java/inha/tnt/hbc/domain/room/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package inha.tnt.hbc.domain.room.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import inha.tnt.hbc.domain.room.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}

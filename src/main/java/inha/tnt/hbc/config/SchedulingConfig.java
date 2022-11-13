9bc50bf4c694e6f5c7c9d53ce0e9c7ae332437b9
seonpilKim <ksp970306@gmail.com>
2022-11-13 23:33:16 +0900

    Feat: 스케줄링 enable 설정 추가

---
+++ src/main/java/inha/tnt/hbc/config/SchedulingConfig.java
@@ -0,0 +1,10 @@
+package inha.tnt.hbc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SchedulingConfig {
+
+}

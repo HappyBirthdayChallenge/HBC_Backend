59182de9b202f93393b29a5144366d1576dd9bbd
seonpilKim <ksp970306@gmail.com>
2022-11-13 23:36:58 +0900

    Feat: 파티룸 생성 대상 회원 찾는 메소드 추가

--- src/main/java/inha/tnt/hbc/domain/member/service/MemberService.java
+++ src/main/java/inha/tnt/hbc/domain/member/service/MemberService.java
@@ -2,15 +2,19 @@ package inha.tnt.hbc.domain.member.service;
 
 import static inha.tnt.hbc.domain.member.entity.MemberRoles.*;
 import static inha.tnt.hbc.model.ErrorCode.*;
+import static inha.tnt.hbc.util.Constants.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import inha.tnt.hbc.domain.member.entity.Member;
 import inha.tnt.hbc.domain.member.repository.MemberRepository;
-import inha.tnt.hbc.exception.EntityNotFoundException;
 import inha.tnt.hbc.domain.member.vo.BirthDate;
 import inha.tnt.hbc.domain.member.vo.ProfileImage;
+import inha.tnt.hbc.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -65,6 +69,14 @@ public class MemberService {
 	public Member findByNameAndPhoneAndUsername(String name, String phone, String username) {
 		return memberRepository.findByNameAndPhoneAndUsername(name, phone, username)
 			.orElseThrow(() -> new EntityNotFoundException(MEMBER_UNFOUNDED));
+	}
+
+	@Transactional(readOnly = true)
+	public List<Member> findAllWhoCanCreateRoom() {
+		final LocalDate roomCreateDate = LocalDate.now().plusDays(DAYS_BEFORE_ROOM_CREATION);
+		final int month = roomCreateDate.getMonthValue();
+		final int date = roomCreateDate.getDayOfMonth();
+		return memberRepository.findAllByBirthDay(month, date);
 	}
 
 }

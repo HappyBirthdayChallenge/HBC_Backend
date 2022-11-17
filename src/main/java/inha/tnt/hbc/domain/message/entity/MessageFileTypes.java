f2fadb3e3bd11448c45fd38972ae18bd75e55742
seonpilKim <ksp970306@gmail.com>
2022-11-17 16:18:44 +0900

    Feat: maxMB 필드 제거, 비즈니스 메소드 추가
    MultipartFile 타입이 지원하는 Image, Video, Audio 타입인지 검사하는 메소드 생성

--- src/main/java/inha/tnt/hbc/domain/message/entity/MessageFileTypes.java
+++ src/main/java/inha/tnt/hbc/domain/message/entity/MessageFileTypes.java
@@ -1,15 +1,52 @@
 package inha.tnt.hbc.domain.message.entity;
 
+import static inha.tnt.hbc.util.Constants.*;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum MessageFileTypes {
-	JPG(10), JPEG(10), PNG(10), GIF(10),
-	MP4(500),
-	M4A(50),
+	JPG, JPEG, PNG, GIF,
+	MP4,
+	M4A,
 	;
 
-	private final int maxMB;
+	public static boolean isImage(MultipartFile multipartFile) {
+		validate(multipartFile);
+		final String extension = extractExtension(multipartFile);
+		return isEqual(extension, JPG) || isEqual(extension, JPEG) || isEqual(extension, PNG)
+			|| isEqual(extension, GIF);
+	}
+
+	public static boolean isVideo(MultipartFile multipartFile) {
+		validate(multipartFile);
+		final String extension = extractExtension(multipartFile);
+		return isEqual(extension, MP4);
+	}
+
+	public static boolean isAudio(MultipartFile multipartFile) {
+		validate(multipartFile);
+		final String extension = extractExtension(multipartFile);
+		return isEqual(extension, M4A);
+	}
+
+	private static String extractExtension(MultipartFile multipartFile) {
+		final String originalFilename = multipartFile.getOriginalFilename();
+		return originalFilename.substring(originalFilename.lastIndexOf(DOT) + 1).toUpperCase();
+	}
+
+	private static boolean isEqual(String extension, MessageFileTypes type) {
+		return extension.equals(type.name());
+	}
+
+	private static void validate(MultipartFile multipartFile) {
+		if (multipartFile.isEmpty()) {
+			throw new RuntimeException();
+		}
+	}
+
 }

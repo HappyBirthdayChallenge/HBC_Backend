09d5842079897d86be0becfb65e30b6a9d986e0c
seonpilKim <ksp970306@gmail.com>
2022-11-22 17:31:08 +0900

    Feat: 메시지 업로드, 파일 업로드 시 본인이 생성한 메시지 여부에 대한 예외처리 추가

--- src/main/java/inha/tnt/hbc/application/file/service/FileService.java
+++ src/main/java/inha/tnt/hbc/application/file/service/FileService.java
@@ -11,12 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 import inha.tnt.hbc.application.file.dto.LocalFile;
 import inha.tnt.hbc.application.file.exception.CannotAttachFileToDeletedMessage;
 import inha.tnt.hbc.application.file.exception.CannotAttachFileToWrittenMessage;
+import inha.tnt.hbc.domain.member.entity.Member;
 import inha.tnt.hbc.domain.message.entity.Message;
 import inha.tnt.hbc.domain.message.service.MessageFileRedisService;
 import inha.tnt.hbc.domain.message.service.MessageService;
 import inha.tnt.hbc.infra.aws.S3Uploader;
 import inha.tnt.hbc.model.file.dto.FileUploadResponse;
 import inha.tnt.hbc.util.FileUtils;
+import inha.tnt.hbc.util.SecurityContextUtils;
 
 @Slf4j
 @Service
@@ -27,9 +29,11 @@ public class FileService {
 	private final S3Uploader s3Uploader;
 	private final MessageService messageService;
 	private final MessageFileRedisService messageFileRedisService;
+	private final SecurityContextUtils securityContextUtils;
 
 	public FileUploadResponse uploadToS3(MultipartFile multipartFile, Long messageId) {
-		final Message message = messageService.findById(messageId);
+		final Member member = securityContextUtils.takeoutMember();
+		final Message message = messageService.findByIdAndMember(messageId, member);
 		if (message.getStatus().equals(DELETED)) {
 			throw new CannotAttachFileToDeletedMessage();
 		}
--- src/main/java/inha/tnt/hbc/application/message/service/MessageFacadeService.java
+++ src/main/java/inha/tnt/hbc/application/message/service/MessageFacadeService.java
@@ -18,6 +18,7 @@ import inha.tnt.hbc.domain.message.service.MessageService;
 import inha.tnt.hbc.domain.room.entity.Room;
 import inha.tnt.hbc.domain.room.service.RoomService;
 import inha.tnt.hbc.model.message.dto.CreateMessageResponse;
+import inha.tnt.hbc.model.message.dto.InquiryMessageResponse;
 import inha.tnt.hbc.model.message.dto.MessageRequest;
 import inha.tnt.hbc.util.SecurityContextUtils;
 
@@ -43,7 +44,7 @@ public class MessageFacadeService {
 	@Transactional
 	public void uploadMessage(MessageRequest request) {
 		final Member member = securityContextUtils.takeoutMember();
-		final Message message = messageService.findById(request.getMessageId());
+		final Message message = messageService.findByIdAndMember(request.getMessageId(), member);
 		final List<MessageFileDto> messageFileDtos = messageFileRedisService.getAll(message.getId(),
 			request.getFileIds());
 		messageFileService.saveAll(message, messageFileDtos);

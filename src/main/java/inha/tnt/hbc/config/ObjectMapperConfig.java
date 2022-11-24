2ddb2620aaa93463c4852be3d0f9fd51381e0d16
seonpilKim <ksp970306@gmail.com>
2022-11-24 16:16:39 +0900

    Feat: jackson-datatype-jsr310 의존성 추가 및 JavaTimeModule 등록

--- src/main/java/inha/tnt/hbc/config/ObjectMapperConfig.java
+++ src/main/java/inha/tnt/hbc/config/ObjectMapperConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class ObjectMapperConfig {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		return objectMapper;
 	}
 
 }

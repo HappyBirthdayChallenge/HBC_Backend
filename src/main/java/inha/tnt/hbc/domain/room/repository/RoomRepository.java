a2a898331085fce6a2ea10a62280dcd553074047
seonpilKim <ksp970306@gmail.com>
2022-11-24 22:18:15 +0900

    Refactor: 조회 최적화 Transactional 어노테이션 추가

--- src/main/java/inha/tnt/hbc/domain/room/repository/RoomRepository.java
+++ src/main/java/inha/tnt/hbc/domain/room/repository/RoomRepository.java
@@ -3,12 +3,14 @@ package inha.tnt.hbc.domain.room.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import inha.tnt.hbc.domain.room.entity.Room;
 import inha.tnt.hbc.domain.room.repository.jdbc.RoomRepositoryJdbc;
 
 public interface RoomRepository extends JpaRepository<Room, Long>, RoomRepositoryJdbc {
 
+	@Transactional(readOnly = true)
 	List<Room> findAllByMemberId(Long memberId);
 
 }

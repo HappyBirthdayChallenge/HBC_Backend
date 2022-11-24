18ec1df9fad7cf1174484201c6fb0caa602cdbbf
seonpilKim <ksp970306@gmail.com>
2022-11-24 16:29:32 +0900

    Feat: 상속 제거

--- src/main/java/inha/tnt/hbc/domain/message/repository/MessageFileRepository.java
+++ src/main/java/inha/tnt/hbc/domain/message/repository/MessageFileRepository.java
@@ -3,8 +3,7 @@ package inha.tnt.hbc.domain.message.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import inha.tnt.hbc.domain.message.entity.MessageFile;
-import inha.tnt.hbc.domain.message.repository.jdbc.MessageFileRepositoryJdbc;
 
-public interface MessageFileRepository extends JpaRepository<MessageFile, Long>, MessageFileRepositoryJdbc {
+public interface MessageFileRepository extends JpaRepository<MessageFile, Long> {
 
 }

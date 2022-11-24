d51fac355d59231372b541844cd70a3b9aa4a5a6
seonpilKim <ksp970306@gmail.com>
2022-11-25 02:08:31 +0900

    Fix: Value 직렬화 Jackson 라이브러리 사용

--- src/main/java/inha/tnt/hbc/config/RedisConfig.java
+++ src/main/java/inha/tnt/hbc/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -25,13 +26,13 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, ?> redisTemplate() {
+	public RedisTemplate<String, Object> redisTemplate() {
 		final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
 		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-		redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
 		redisTemplate.afterPropertiesSet();
 		return redisTemplate;
 	}

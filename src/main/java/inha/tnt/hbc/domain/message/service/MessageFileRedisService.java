b1cb1225b957cfaf027cec1d0c7ab5e06aaee821
seonpilKim <ksp970306@gmail.com>
2022-11-24 16:30:44 +0900

    Fix: redis key 제거 메소드 정정 및 key-value 저장 메소드 리팩토링

--- src/main/java/inha/tnt/hbc/domain/message/service/MessageFileRedisService.java
+++ src/main/java/inha/tnt/hbc/domain/message/service/MessageFileRedisService.java
@@ -1,22 +1,20 @@
 package inha.tnt.hbc.domain.message.service;
 
-import static inha.tnt.hbc.model.ErrorCode.*;
 import static inha.tnt.hbc.util.Constants.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import inha.tnt.hbc.application.file.dto.LocalFile;
 import inha.tnt.hbc.domain.message.dto.MessageFileDto;
-import inha.tnt.hbc.exception.EntityNotFoundException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MessageFileRedisService {
@@ -24,29 +22,14 @@ public class MessageFileRedisService {
 	private static final String REDIS_PREFIX_KEY = "mf";
 	private final RedisTemplate<String, Object> redisTemplate;
 
-	public void save(Long messageId, String fileId, LocalFile localFile) {
-		final Map<String, Map<String, String>> hashKey = new HashMap<>();
-		hashKey.put(fileId, MessageFileDto.of(localFile).convertToMap());
+	public void save(Long messageId, Long fileId, LocalFile localFile) {
+		final Map<String, Object> hashKey = new HashMap<>();
+		hashKey.put(fileId.toString(), MessageFileDto.of(localFile).convertToMap());
 		redisTemplate.opsForHash().putAll(generateRedisKey(messageId), hashKey);
 	}
 
-	public List<MessageFileDto> getAll(Long messageId, List<String> fileIds) {
-		final String key = generateRedisKey(messageId);
-		return fileIds.stream()
-			.map(hashKey -> {
-				final Object value = redisTemplate.opsForHash().get(key, hashKey);
-				if (value == null) {
-					throw new EntityNotFoundException(FILE_UNFOUNDED); // TODO: field error 추가
-				}
-				return value;
-			})
-			.map(value -> (Map<String, String>)value)
-			.map(MessageFileDto::of)
-			.collect(Collectors.toList());
-	}
-
 	public void delete(Long messageId) {
-		redisTemplate.opsForHash().delete(generateRedisKey(messageId)); // TODO: 수정
+		redisTemplate.delete(generateRedisKey(messageId));
 	}
 
 	private String generateRedisKey(Long messageId) {

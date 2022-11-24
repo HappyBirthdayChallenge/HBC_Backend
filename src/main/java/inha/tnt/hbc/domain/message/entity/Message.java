7552d0f091425f3d6e1b38d257932ef5a0186d52
seonpilKim <ksp970306@gmail.com>
2022-11-24 15:59:35 +0900

    Refactor: 변경된 메소드명 업데이트

--- src/main/java/inha/tnt/hbc/domain/message/entity/Message.java
+++ src/main/java/inha/tnt/hbc/domain/message/entity/Message.java
@@ -66,7 +66,7 @@ public class Message extends BaseEntity {
 
 	public List<String> getFileUris() {
 		return messageFiles.stream()
-			.map(MessageFile::getFileUri)
+			.map(MessageFile::getS3FileUri)
 			.collect(Collectors.toList());
 	}
 
